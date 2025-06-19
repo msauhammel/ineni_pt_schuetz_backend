4d0c474b9d74c69b643f7436a36fb5969fb1c9d7
Marco Nika <112717705+msauhammel@users.noreply.github.com>
2025-06-19 10:16:26 +0200

    try fix helm chart

---
+++ src/main/java/at/hochschule/ineni_pt/employee/HealthController.java
@@ -0,0 +1,13 @@
+package at.hochschule.ineni_pt.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+}
